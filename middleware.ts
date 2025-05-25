eac9b1303457769f6230067f8bd5c2c3bd3f0569
Pulkit Garg <pulkitgargbnl@gmail.com>
2025-05-25 12:26:14 +0530

    fix: url slug not working

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/', '/s/:path*']);
+const isPublicRoute = createRouteMatcher(['/', '/s/([^/]+)']);
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
